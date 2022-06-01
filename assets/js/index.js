6060502a235733dc6d29c19d803a4e33f2ac30f8
SebaChuaqui <schuaquicano@gmail.com>
2022-06-01 19:35:42 -0300

    Artes Marciales IX

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,4 +6,28 @@ document.getElementById("btnRegistrar").addEventListener("click", () => {
     let nombre = document.getElementById("nombre");
     let raza = document.getElementById("raza");
     let previewElement = document.getElementById("preview");
-})
+    let ImagenSrcBg = previewElement.style.backgroundImage;
+    let imgSrc = ImagenSrcBg.slice(5, ImagenSrcBg.length - 2);
+    let ki = document.getElementById("poderPelea");
+
+    let nuevoParticipante;
+
+    if (raza.value == "Saiyajin") {
+        nuevoParticipante = new Saiyajin(
+            nombre.value,
+            imgSrc.value,
+            ki.value,
+            raza.value
+        );
+    } else if (raza.value == "Humano") {
+        nuevoParticipante = new Humano(
+            nombre.value,
+            imgSrc.value,
+            ki.value,
+            raza.value
+        );
+    }
+
+    participantes.push(nuevoParticipante);
+    console.log(participantes);
+});
