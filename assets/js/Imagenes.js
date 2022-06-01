51c3f036651ec00240c31dff8a33d8d95e135e1f
SebaChuaqui <schuaquicano@gmail.com>
2022-06-01 17:30:57 -0300

    Artes Marciales II

--- assets/js/Imagenes.js
+++ assets/js/Imagenes.js
@@ -1,7 +1,21 @@
 import Personajes from "./Consultas.js";
 
-document.getElementById("buttonImages").addEventListener("click", async() => {
+document.getElementById("buttonImages").addEventListener("click", async () => {
     const { personajes } = await Personajes.getData();
     console.log(personajes);
     const pj = document.getElementById("nombre").value;
+    const imagenesPjTemplate = personajes
+        .find((p) => p.name == pj)
+        .imagenes.map((i) => `<img width="200" src="assets/imgs/${pj}/${i}" />`)
+        .join("");
+
+    document.getElementsByClassName("personajes"
+    )[0].innerHTML = imagenesPjTemplate;
+
+    document.querySelectorAll(".personajes img").forEach((i) => {
+        i.addEventListener("click", (e) => {
+            $("#imagenModal").modal("toggle");
+            const imagenSrc = e.target.src;
+        });
+    });
 });
