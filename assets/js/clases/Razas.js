ff2deae22b236f7816aacaab74bc7b8fc291a163
SebaChuaqui <schuaquicano@gmail.com>
2022-06-01 18:48:40 -0300

    Artes Marciales V

---
+++ assets/js/clases/Razas.js
@@ -0,0 +1,24 @@
+import Personaje from "./personaje.js";
+
+class Saiyajin extends Personaje {
+    constructor(nombre, img, poder, raza) {
+        super(nombre, img, poder, raza)
+    }
+
+    Transformacion() {
+        let poder = this.getPoder();
+        this.setPoder(parseInt(poder * 1.8));
+    }
+}
+
+    class Humano extends Personaje {
+    constructor(nombre, img, poder, raza) {
+        super(nombre, img, poder, raza)
+    }
+
+    Coraje() {
+        let poder = this.getPoder();
+        this.setPoder(parseInt(poder * 1.2));
+    }
+}
+
